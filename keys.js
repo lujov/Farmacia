ff749d1a6d4a309b84160808e9916a36d9296598
lujov <lucia.rodriguezjov@gmail.com>
2021-04-24 19:32:05 -0300

    form

--- keys.js
+++ keys.js
@@ -1,19 +1,19 @@
 module.exports = {
 
-    database: {
-            host: 'us-cdbr-east-03.cleardb.com',
-            user: 'bd3a259f4a4ff9',
-            password:'a92d0042',
-            database: 'heroku_5e04ceac3204daf',
-            port:3306
-    }
+    // database: {
+    //         host: 'us-cdbr-east-03.cleardb.com',
+    //         user: 'bd3a259f4a4ff9',
+    //         password:'a92d0042',
+    //         database: 'heroku_5e04ceac3204daf',
+    //         port:3306
+    // }
 
-//     database: {
-//         host: 'localhost',
-//         user: 'root',
-//         password:'',
-//         database: 'bd_productos',
-//         port:3306
-// }
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password:'',
+        database: 'bd_productos',
+        port:3306
+}
 
 }
